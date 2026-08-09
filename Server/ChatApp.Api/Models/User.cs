5cf41c063648c24053102499fea196bf8680c7af
Artikiwi <artikiwi@gmail.com>
2026-08-09 16:00:18 +0300

    Add User model for user management

---
+++ Server/ChatApp.Api/Models/User.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+
+namespace ChatApp.Api.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string UserName { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
