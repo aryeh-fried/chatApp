9a692ea955bbad8905a0ed3c1d0eee0269ef200a
Artikiwi <artikiwi@gmail.com>
2026-08-11 09:48:09 +0300

    Implement user retrieval and creation endpoints in UsersController; change User Id type to int

--- Server/ChatApp.Api/Controllers/UsersController.cs
+++ Server/ChatApp.Api/Controllers/UsersController.cs
@@ -1,3 +1,4 @@
+using ChatApp.Api.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ChatApp.Api.Controllers;
@@ -6,9 +7,38 @@ namespace ChatApp.Api.Controllers;
 [Route("api/[controller]")]
 public class UsersController : ControllerBase
 {
-    [HttpGet]
-    public string GetUsers()
+    private static readonly List<User> _users = new()
     {
-        return "Get Users EndPoint working!";
+        new() { Id = 1, UserName = "JohnDoe", Email = "john.doe@example.com", Password = "password" },
+        new() { Id = 2, UserName = "JaneSmith", Email = "jane.smith@example.com", Password = "password" },
+        new() { Id = 3, UserName = "AliceJones", Email = "alice.jones@example.com", Password = "password" },
+        new() { Id = 4, UserName = "BobBrown", Email = "bob.brown@example.com", Password = "password" },
+        new() { Id = 5, UserName = "CharlieBlack", Email = "charlie.black@example.com", Password = "password" }
+    };
+
+    [HttpGet]
+    public List<User> GetUsers()
+    {
+
+        return _users;
+    }
+    [HttpGet("{id}")]
+    public ActionResult<User> GetUser(int id)
+    {
+        var user = _users.FirstOrDefault(u => u.Id == id);
+        if (user == null)
+        {
+           return NotFound();
+        }
+
+
+        return user;
+    }
+    [HttpPost]
+    public ActionResult<User> CreateUser(User AddUser)
+    {
+        AddUser.Id = _users.Max(u => u.Id) + 1;
+        _users.Add(AddUser);
+        return CreatedAtAction(nameof(GetUser), new { id = AddUser.Id }, AddUser);
     }
 }   
--- Server/ChatApp.Api/Models/User.cs
+++ Server/ChatApp.Api/Models/User.cs
@@ -5,7 +5,7 @@ namespace ChatApp.Api.Models
 {
     public class User
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
         public string UserName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
