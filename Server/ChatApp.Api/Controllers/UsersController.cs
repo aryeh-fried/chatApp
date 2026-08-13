b3752fdae85d124e9c6aa6120ae9f0401dc4996a
Artikiwi <artikiwi@gmail.com>
2026-08-13 15:19:46 +0300

    Remove static user list from UsersController to rely on IUserService for user management

--- Server/ChatApp.Api/Controllers/UsersController.cs
+++ Server/ChatApp.Api/Controllers/UsersController.cs
@@ -13,22 +13,16 @@ public class UsersController : ControllerBase
     {
         _userService = userService;
     }
+    
     private readonly IUserService _userService;
-    private static readonly List<User> _users = new()
-    {
-        new() { Id = 1, UserName = "JohnDoe", Email = "john.doe@example.com", Password = "password" },
-        new() { Id = 2, UserName = "JaneSmith", Email = "jane.smith@example.com", Password = "password" },
-        new() { Id = 3, UserName = "AliceJones", Email = "alice.jones@example.com", Password = "password" },
-        new() { Id = 4, UserName = "BobBrown", Email = "bob.brown@example.com", Password = "password" },
-        new() { Id = 5, UserName = "CharlieBlack", Email = "charlie.black@example.com", Password = "password" }
-    };
+   
 
     [HttpGet]
     public async Task<ActionResult<List<User>>> GetUsers()
     {
         return await _userService.GetAllUsers();
     }
-    
+
     [HttpGet("{id}")]
     public async Task<ActionResult<User>> GetUser(int id)
     {
@@ -38,9 +32,9 @@ public class UsersController : ControllerBase
            return NotFound();
         }
 
-
         return user;
     }
+
     [HttpPost]
     public async Task<ActionResult<User>> CreateUser(CreateUserDto AddUser)
     {
