051a1d6e8745cdf8350c98aa1f6990ad36010a78
Artikiwi <artikiwi@gmail.com>
2026-08-13 13:47:34 +0300

    Add ChatAppDbContext for Entity Framework Core integration and update appsettings.json for connection strings

---
+++ Server/ChatApp.Api/Data/ChatAppDbContext.cs
@@ -0,0 +1,15 @@
+using ChatApp.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatApp.Api.Data
+{
+    public class ChatAppDbContext : DbContext
+    {
+        public ChatAppDbContext(DbContextOptions<ChatAppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        
+    }
+}
